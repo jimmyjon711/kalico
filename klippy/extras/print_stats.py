a365d79a421f8dd637e5522085a861236295a0ba
Ingo Donasch <idonasch@users.noreply.github.com>
2025-05-31 19:06:44 +0200

    print_stats: Fix for filament statistics bug  in print_stats.py for toolchangers (#6946)
    added extruder:activate_extruder event hook to print_stats.py to update self.last_epos
    
    Signed-off-by: Ingo Donasch <ingo@donasch.net>

--- klippy/extras/print_stats.py
+++ klippy/extras/print_stats.py
@@ -18,6 +18,11 @@ class PrintStats:
             self.cmd_SET_PRINT_STATS_INFO,
             desc=self.cmd_SET_PRINT_STATS_INFO_help,
         )
+        self.printer.register_event_handler("extruder:activate_extruder",
+                                            self._handle_activate_extruder)
+    def _handle_activate_extruder(self):
+        gc_status = self.gcode_move.get_status()
+        self.last_epos = gc_status['position'].e
 
     def _update_filament_usage(self, eventtime):
         gc_status = self.gcode_move.get_status(eventtime)
